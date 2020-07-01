b5766a04e858d24cd8b8b33b4e4f09707f4e49d5
Alex Lee <a.s.lee@swansea.ac.uk>
2020-07-01 21:50:52 +0100

    updated to have text box input

---
+++ lazycomment.py
@@ -0,0 +1,31 @@
+import sublime, sublime_plugin
+
+# Extends TextCommand so that run() receives a View to modify.
+class StuCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        def on_done(input_string):
+            #self.view.run_command("move_to", {"to": "bof"})
+            transf = transform_string(input_string)
+            self.view.run_command("insert", {"characters": transf})
+
+        def on_change(input_string):
+            print("Input changed: %s" % input_string)
+
+        def on_cancel():
+            print("User cancelled the input")
+
+        def transform_string(transform):
+            textLen = len(transform)
+            neededDash = (78 - textLen) / 2
+            return '# ' + transform + ' ' + " ".join("-"*int(neededDash))
+
+        for region in self.view.sel():
+            if not region.empty():
+                # Get the selected text
+                s = self.view.substr(region)
+                transf = transform_string(s)
+                # Replace the selection with transformed text
+                self.view.replace(edit, region, transf)
+            if region.empty():
+                window = self.view.window()
+                window.show_input_panel("Text to Insert:", "Hello, World!", on_done, on_change, on_cancel)
